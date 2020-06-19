093997fc2bcac714c7e4908058f24582ed5b8210
Kyle Lin <kylejlin.us@gmail.com>
2020-06-19 16:41:05 -0700

    Write first draft of neural network

---
+++ src/matrix.ts
@@ -0,0 +1,168 @@
+export class Matrix {
+  static random(rows: number, columns: number): Matrix {
+    const size = rows * columns;
+    const data = new Array(size);
+    for (let i = 0; i < size; i++) {
+      data[i] = Math.random();
+    }
+    return new Matrix(rows, columns, data);
+  }
+
+  static zero(rows: number, columns: number): Matrix {
+    const data = new Array(rows * columns).fill(0);
+    return new Matrix(rows, columns, data);
+  }
+
+  public readonly rows: number;
+  public readonly columns: number;
+  private data: number[];
+
+  private constructor(rows: number, columns: number, data: number[]) {
+    this.rows = rows;
+    this.columns = columns;
+    this.data = data;
+  }
+
+  clone(): Matrix {
+    return new Matrix(this.rows, this.columns, this.data.slice());
+  }
+
+  mutMultiplyScalar(n: number): void {
+    const size = this.data.length;
+    for (let i = 0; i < size; i++) {
+      this.data[i] *= n;
+    }
+  }
+
+  mutAdd(other: Matrix): void {
+    if (!(other.rows === this.rows && other.columns === this.columns)) {
+      throw new TypeError(
+        "Cannot add a " +
+          this.rows +
+          "x" +
+          this.columns +
+          " to a " +
+          other.rows +
+          "x" +
+          other.columns +
+          " matrix."
+      );
+    }
+
+    const size = this.data.length;
+    for (let i = 0; i < size; i++) {
+      this.data[i] += other.data[i];
+    }
+  }
+
+  immutSubtract(other: Matrix): Matrix {
+    if (!(other.rows === this.rows && other.columns === this.columns)) {
+      throw new TypeError(
+        "Cannot add a " +
+          this.rows +
+          "x" +
+          this.columns +
+          " matrix to a " +
+          other.rows +
+          "x" +
+          other.columns +
+          " matrix."
+      );
+    }
+
+    const clone = this.clone();
+    const size = clone.data.length;
+    for (let i = 0; i < size; i++) {
+      clone.data[i] -= other.data[i];
+    }
+    return clone;
+  }
+
+  immutMultiply(other: Matrix): Matrix {
+    if (this.columns !== other.rows) {
+      throw new TypeError(
+        "Cannot multiply a " +
+          this.rows +
+          "x" +
+          this.columns +
+          " matrix with a " +
+          other.rows +
+          "x" +
+          other.columns +
+          " matrix."
+      );
+    }
+
+    const product = Matrix.zero(this.rows, other.columns);
+
+    const thisData = this.data;
+    const otherData = other.data;
+    const productData = product.data;
+    const thisRows = this.rows;
+    const otherColumns = other.columns;
+    const thisColumns = this.columns;
+    const productColumns = product.columns;
+
+    for (let thisR = 0; thisR < thisRows; thisR++) {
+      for (let otherC = 0; otherC < otherColumns; otherC++) {
+        for (let thisC = 0; thisC < thisColumns; thisC++) {
+          productData[thisR * productColumns + otherC] +=
+            thisData[thisR * thisColumns + thisC] *
+            otherData[thisC * otherColumns + otherC];
+        }
+      }
+    }
+    return product;
+  }
+
+  mutHadamard(other: Matrix): void {
+    if (!(other.rows === this.rows && other.columns === this.columns)) {
+      throw new TypeError(
+        "Cannot take the Hadamard product of a " +
+          this.rows +
+          "x" +
+          this.columns +
+          " matrix and a " +
+          other.rows +
+          "x" +
+          other.columns +
+          " matrix."
+      );
+    }
+
+    const size = this.data.length;
+    for (let i = 0; i < size; i++) {
+      this.data[i] *= other.data[i];
+    }
+  }
+
+  immutTranspose(): Matrix {
+    const transposed = new Matrix(this.columns, this.rows, this.data.slice());
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        if (r === c) {
+          continue;
+        }
+
+        transposed.data[r * transposed.columns + c] = this.data[
+          c * this.columns + r
+        ];
+      }
+    }
+    return transposed;
+  }
+
+  rowMajorOrderEntries(): readonly number[] {
+    return this.data;
+  }
+
+  immutApplyElementwise(f: (entry: number) => number): Matrix {
+    const clone = this.clone();
+    const cloneData = clone.data;
+    const size = cloneData.length;
+    for (let i = 0; i < size; i++) {
+      cloneData[i] = f(cloneData[i]);
+    }
+    return clone;
+  }
+}
---
+++ src/network.ts
@@ -0,0 +1,252 @@
+import { Matrix } from "./matrix";
+
+export class Network {
+  private layers: number;
+  private weights: MatrixMap;
+  private biases: MatrixMap;
+  private log: (message: string) => void;
+
+  constructor(sizes: number[], log?: (message: string) => void) {
+    this.layers = sizes.length;
+
+    this.weights = [];
+    this.biases = [];
+    for (let outputLayer = 1; outputLayer < sizes.length; outputLayer++) {
+      const inputLayer = outputLayer - 1;
+      const outputLayerSize = sizes[outputLayer];
+      const inputLayerSize = sizes[inputLayer];
+      this.weights[outputLayer] = Matrix.random(
+        outputLayerSize,
+        inputLayerSize
+      );
+      this.biases[outputLayer] = Matrix.random(outputLayerSize, 1);
+    }
+
+    this.log = log || (() => {});
+  }
+
+  stochasticGradientDescent(
+    trainingData: LabeledTrainingImage[],
+    miniBatchSize: number,
+    epochs: number,
+    learningRate: number,
+    testData?: LabeledTestImage[]
+  ): void {
+    for (let epoch = 0; epoch < epochs; epoch++) {
+      const miniBatches = divideIntoMiniBatches(trainingData, miniBatchSize);
+      for (const miniBatch of miniBatches) {
+        const { weightGradients, biasGradients } = this.getAverageGradients(
+          miniBatch
+        );
+
+        for (let i = 1; i < this.layers; i++) {
+          weightGradients[i].mutMultiplyScalar(learningRate);
+          biasGradients[i].mutMultiplyScalar(learningRate);
+
+          this.weights[i].mutAdd(weightGradients[i]);
+          this.biases[i].mutAdd(biasGradients[i]);
+        }
+      }
+
+      if (testData !== undefined) {
+        const numCorrect = this.getCorrectClassifications(testData);
+        this.log(
+          "Epoch " +
+            epoch +
+            ": " +
+            numCorrect +
+            " / " +
+            testData.length +
+            " correct"
+        );
+      }
+    }
+  }
+
+  private getAverageGradients(miniBatch: LabeledTrainingImage[]): Gradients {
+    const weightGradients = this.getZeroMatricesForWeightGradients();
+    const biasGradients = this.getZeroMatricesForBiasGradients();
+
+    for (const image of miniBatch) {
+      const imageGradients = this.getGradients(image);
+
+      for (let i = 1; i < this.layers; i++) {
+        weightGradients[i].mutAdd(imageGradients.weightGradients[i]);
+        biasGradients[i].mutAdd(imageGradients.biasGradients[i]);
+      }
+    }
+
+    for (let i = 1; i < this.layers; i++) {
+      weightGradients[i].mutMultiplyScalar(1 / miniBatch.length);
+      biasGradients[i].mutMultiplyScalar(1 / miniBatch.length);
+    }
+
+    return { weightGradients, biasGradients };
+  }
+
+  private getZeroMatricesForWeightGradients(): MatrixMap {
+    const matrices: MatrixMap = [];
+    for (let i = 1; i < this.layers; i++) {
+      const weightMatrix = this.weights[i];
+      matrices[i] = Matrix.zero(weightMatrix.rows, weightMatrix.columns);
+    }
+    return matrices;
+  }
+
+  private getZeroMatricesForBiasGradients(): MatrixMap {
+    const matrices: MatrixMap = [];
+    for (let i = 1; i < this.layers; i++) {
+      const biasMatrix = this.biases[i];
+      matrices[i] = Matrix.zero(biasMatrix.rows, biasMatrix.columns);
+    }
+    return matrices;
+  }
+
+  private getGradients(image: LabeledTrainingImage): Gradients {
+    const { weightedSums, activations } = this.performForwardPass(image.inputs);
+    const errors: MatrixMap = [];
+    const weightGradients: MatrixMap = [];
+    const biasGradients: MatrixMap = [];
+
+    const lastLayerError = weightedSums[this.layers - 1].immutApplyElementwise(
+      sigmaPrime
+    );
+    lastLayerError.mutHadamard(
+      this.getLastLayerCostDerivative(
+        activations[this.layers - 1],
+        image.outputs
+      )
+    );
+    errors[this.layers - 1] = lastLayerError;
+
+    for (let i = this.layers - 2; i >= 1; i--) {
+      const error = this.weights[i + 1]
+        .immutTranspose()
+        .immutMultiply(errors[i + 1]);
+      error.mutHadamard(weightedSums[i].immutApplyElementwise(sigmaPrime));
+      errors[i] = error;
+
+      weightGradients[i] = error.immutMultiply(
+        activations[i - 1].immutTranspose()
+      );
+
+      biasGradients[i] = error;
+    }
+
+    return { weightGradients, biasGradients };
+  }
+
+  private performForwardPass(inputs: Matrix): WeightedSumsAndActivations {
+    const weightedSums: MatrixMap = [];
+    const activations: MatrixMap = [inputs];
+
+    for (let outputLayer = 1; outputLayer < this.layers; outputLayer++) {
+      const inputLayer = outputLayer - 1;
+      const weightedSum = this.weights[outputLayer].immutMultiply(
+        activations[inputLayer]
+      );
+      weightedSum.mutAdd(this.biases[outputLayer]);
+      weightedSums[outputLayer] = weightedSum;
+      activations[outputLayer] = weightedSum.immutApplyElementwise(sigma);
+    }
+    return { weightedSums, activations };
+  }
+
+  private getLastLayerCostDerivative(
+    actualOutput: Matrix,
+    expectedOutput: Matrix
+  ): Matrix {
+    return actualOutput.immutSubtract(expectedOutput);
+  }
+
+  private getCorrectClassifications(testData: LabeledTestImage[]): number {
+    let correctClassifications = 0;
+    for (const image of testData) {
+      const { activations } = this.performForwardPass(image.inputs);
+      const prediction = argmax(
+        activations[this.layers - 1].rowMajorOrderEntries()
+      );
+      if (prediction === image.label) {
+        correctClassifications++;
+      }
+    }
+    return correctClassifications;
+  }
+}
+
+export interface MatrixMap {
+  [layer: number]: Matrix;
+}
+
+export interface LabeledTrainingImage {
+  inputs: Matrix;
+  outputs: Matrix;
+}
+
+export interface LabeledTestImage {
+  inputs: Matrix;
+  label: Digit;
+}
+
+export type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export interface Gradients {
+  weightGradients: MatrixMap;
+  biasGradients: MatrixMap;
+}
+
+export interface WeightedSumsAndActivations {
+  weightedSums: MatrixMap;
+  activations: MatrixMap;
+}
+
+function divideIntoMiniBatches(
+  trainingData: LabeledTrainingImage[],
+  miniBatchSize: number
+): LabeledTrainingImage[][] {
+  shuffle(trainingData);
+  const miniBatches: LabeledTrainingImage[][] = [];
+  for (let i = 0; i < trainingData.length; i += miniBatchSize) {
+    miniBatches.push(trainingData.slice(i, i + miniBatchSize));
+  }
+  return miniBatches;
+}
+
+function shuffle(arr: unknown[]): void {
+  const SHUFFLE_TIMES = 512;
+
+  for (let n = 0; n < SHUFFLE_TIMES; n++) {
+    for (let i = arr.length - 1; i >= 1; i--) {
+      let j = randInt(i + 1);
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+}
+
+function randInt(exclMax: number): number {
+  return Math.floor(Math.random() * exclMax);
+}
+
+function sigma(z: number): number {
+  return 1 / (1 + Math.exp(-z));
+}
+
+function sigmaPrime(z: number): number {
+  const sigmaZ = sigma(z);
+  return sigmaZ * (1 - sigmaZ);
+}
+
+function argmax(arr: readonly number[]): number {
+  let maxIndex = 0;
+  let max = arr[maxIndex];
+  for (let i = 1; i < arr.length; i++) {
+    const value = arr[i];
+    if (value > max) {
+      max = value;
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
