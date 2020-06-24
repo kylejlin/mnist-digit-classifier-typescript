00da2e6956e48eae470c921dd82a6635cf83e9a6
Kyle Lin <kylejlin.us@gmail.com>
2020-06-23 21:14:51 -0700

    Updates

---
+++ precache-manifest.2750f8694b0ac86bcaee5d5c5be4c82d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f326856ae0d921861b3cc2fb96cf5f",
+    "url": "/mnist-digit-classifier-typescript/6586aa469a69db687a96.worker.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/mnist-digit-classifier-typescript/6586aa469a69db687a96.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2e47dafcefc110490be8a2dfa62892",
+    "url": "/mnist-digit-classifier-typescript/b6cf119445366dff338d.worker.js"
+  },
+  {
+    "revision": "d985de5d55e0e6dbadee53aa909f70c1",
+    "url": "/mnist-digit-classifier-typescript/b6cf119445366dff338d.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "0505c2d1e1050f0c311b6000738f46c8",
+    "url": "/mnist-digit-classifier-typescript/index.html"
+  },
+  {
+    "revision": "a9cd5f9c32ef6487b0a9",
+    "url": "/mnist-digit-classifier-typescript/static/css/main.7ff26fab.chunk.css"
+  },
+  {
+    "revision": "9bb9e87d940395781c66",
+    "url": "/mnist-digit-classifier-typescript/static/js/2.d050a2e0.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/mnist-digit-classifier-typescript/static/js/2.d050a2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cd5f9c32ef6487b0a9",
+    "url": "/mnist-digit-classifier-typescript/static/js/main.50c67af4.chunk.js"
+  },
+  {
+    "revision": "2711db2581b18301825d",
+    "url": "/mnist-digit-classifier-typescript/static/js/runtime-main.b834a0b1.js"
+  }
+]);
